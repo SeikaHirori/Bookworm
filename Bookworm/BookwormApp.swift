9a65351b84f1e7cec7cc7f0f07cebd0bcec87490
SeikaHirori <SeikaHirori@outlook.com>
2023-06-02 11:37:16 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Seika Hirori on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2f3a6a46bc5a41b153bb73b505709de53aaa5f99
SeikaHirori <SeikaHirori@outlook.com>
2023-06-04 10:28:55 -0700

    Starting section 1.4: Core Data

---
+++ Bookworm/part1/section4/oneDot4.swift
@@ -0,0 +1,20 @@
+//
+//  oneDot4.swift
+//  Bookworm
+//
+//  Created by Seika Hirori on 6/4/23.
+//
+
+import SwiftUI
+
+struct oneDot4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct oneDot4_Previews: PreviewProvider {
+    static var previews: some View {
+        oneDot4()
+    }
+}
--- _docs/TODO.md
+++ _docs/TODO.md
@@ -5,6 +5,6 @@ Link: [https://www.hackingwithswift.com/100/swiftui/53](https://www.hackingwiths
 
 - [x] Bookworm: Introduction
 - [x] Creating a custom component with @Binding
-- [ ] Accepting multi-line text input with TextEditor
-    - WIP
+- [x] Accepting multi-line text input with TextEditor
 - [ ] How to combine Core Data and SwiftUI
+    - WIP
